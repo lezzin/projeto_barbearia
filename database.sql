5cb634de02a9cab4aeb5998fe88b758b87bc83f6
Leandro Adrian da Silva <103830032+lezzin@users.noreply.github.com>
2024-02-16 18:35:16 -0300

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,48 +1,55 @@
-DROP DATABASE IF EXISTS `barbearia`;
-CREATE DATABASE `barbearia`;
-USE `barbearia`;
-
-CREATE TABLE `user` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(45) NOT NULL,
-    `tel` VARCHAR(45),
-    `password` TEXT NOT NULL,
-    `type` INT NOT NULL DEFAULT 1,
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` text NOT NULL,
+  `tel` varchar(45) DEFAULT NULL,
+  `password` text NOT NULL,
+  `type` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
 );
 
-INSERT INTO `user` (`name`, `password`, `type`) VALUES ("adm", "$2y$10$34zXSGF5yVuDp/Q09rCZnu2FtBNZF.4bZectqBLvM4BGGqFhScXXq", 2);
+-- INSERT INTO `user` (`id`, `name`, `email`, `tel`, `password`, `type`) VALUES
+-- (3, 'adm', 'leandrinsilva22@gmail.com', '123123123', '$2y$10$/BUNPbo/Pp9JSV.cdwznt.LS3Tvs.tyRldXnN2KWj6iH9a895Ds0y', 2);
 
-CREATE TABLE `service` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(45) NOT NULL,
-    `price` DECIMAL(8, 2) NOT NULL,
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS `service` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `price` decimal(8,2) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `unavailable_datetime` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `datetime` DATETIME NOT NULL,
-    PRIMARY KEY(`id`)
+INSERT INTO `service` (`id`, `name`, `price`) VALUES
+(1, 'Acabamento', 10.00),
+(2, 'Cabelo', 30.00),
+(3, 'Barba', 50.00),
+(4, 'Cabelo e barba', 80.00);
+
+CREATE TABLE IF NOT EXISTS `contact_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  `tel` varchar(45) NOT NULL,
+  `whatsapp` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `schedule` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `user` VARCHAR(45) NOT NULL,
-    `tel` VARCHAR(45) NOT NULL,
-    `service_id` INT NOT NULL,
-    `datetime` DATETIME NOT NULL,
-    `message` TEXT NULL,
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS `schedule` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) NOT NULL,
+  `tel` varchar(45) NOT NULL,
+  `email` text NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `message` text DEFAULT NULL,
+  `fk_user` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_schedule_service_id` FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_schedule_user_id` FOREIGN KEY (`fk_user`) REFERENCES `user` (`id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `contact_info` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(45) NOT NULL,
-    `address` VARCHAR(45) NOT NULL,
-    `tel` VARCHAR(45) NOT NULL,
-    `whatsapp` VARCHAR(45) NOT NULL,
-    PRIMARY KEY(`id`)
-)
-
-ALTER TABLE `schedule` ADD CONSTRAINT `fk_schedule_service_id` FOREIGN KEY ( `service_id` ) REFERENCES `service` ( `id` ) ON DELETE CASCADE;
+CREATE TABLE IF NOT EXISTS `unavailable_datetime` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+);
