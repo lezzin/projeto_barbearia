eaac4ebae1c799774e159ccb7823afa7eb429c20
Leandro Adrian da Silva <103830032+lezzin@users.noreply.github.com>
2024-05-12 16:31:17 -0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('BASE_URL', 'http://localhost/projetos/projeto_barbearia/');
-
-define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'barbearia');
