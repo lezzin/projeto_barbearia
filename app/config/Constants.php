bd31e543207d3f91f97cf35e5e3c781bc0f08786
leandro <leandrinsilva22@gmail.com>
2024-06-28 18:05:57 -0300

    remove confidential data

--- app/config/Constants.php
+++ app/config/Constants.php
@@ -3,9 +3,9 @@
 $mode = 'dev';
 
 define('DB_HOST', '127.0.0.1');
-define('DB_USER', $mode === 'dev' ? 'root' : 'id21709051_lzzn');
-define('DB_PASS', $mode === 'dev' ? '' : 'Barbearia#3690');
-define('DB_NAME', $mode === 'dev' ? 'barbershop' : 'id21709051_lzzn_barber');
-define('BASE_URL', $mode === 'dev' ? 'http://localhost/projetos/projeto_barbearia/' : 'https://lzzn-barber.000webhostapp.com/');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'barbershop');
+define('BASE_URL', 'http://localhost/projetos/projeto_barbearia/');
 
 define('PAGE_TITLE', 'Barbearia');
